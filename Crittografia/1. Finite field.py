2f7b9c8d7e2548662fdd865af9da8adc71a5623b
davide zambon <dadezz@protonmail.com>
2022-04-28 01:12:41 +0200

    Update 1. Finite field.py

--- Crittografia/1. Finite field.py
+++ Crittografia/1. Finite field.py
@@ -1,4 +1,4 @@
-###
+"""
 Un campo finito è un campo algebrico in cui vi è un numero finito di elementi. In particolare, 
 Le operazioni di moltiplicazione e addizione sono chiuse nel campo, ovvero sono definite in modo tale
 da ritornare un numero appartenente al campo stesso (per esempio addizione modulare). 
@@ -7,7 +7,7 @@ Chiamo p l'ordine (la grandezza) del campo, che avrà quindi elementi {0, 1, 2, 
 Ovviamente, l'ordine sarà sempre maggiore di un unità rispetto all'elemento più grande. 
 I campi che interessano a noi sono i campi il cui ordine è un numero primo. (notare che un campo finito  
 Ha sempre un ordine che è potenza di un primo). 
-###
+"""
 
 class FieldElement:
 	def __init__(self, num, prime):
